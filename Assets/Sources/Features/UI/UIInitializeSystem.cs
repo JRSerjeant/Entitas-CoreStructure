e8dd10ec50524d71436f397ab3cee1b479408ff0
Jack Serjeant <Jack.Serjeant@gmail.com>
2017-01-14 15:43:57 +0000

    More

---
+++ Assets/Sources/Features/UI/UIInitializeSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Entitas;
+
+class UIInitializeSystem : IInitializeSystem
+{
+    readonly Context _context;
+
+    public UIInitializeSystem(Contexts contexts)
+    {
+        _context = contexts.game;
+    }
+
+
+    public void Initialize()
+    {
+        throw new NotImplementedException();
+    }
+}
+
